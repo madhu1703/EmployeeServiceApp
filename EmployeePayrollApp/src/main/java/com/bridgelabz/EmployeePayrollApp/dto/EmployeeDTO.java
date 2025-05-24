5dc64646834814aa821ed6cf17e354c15bdafdb5
Madhumitha A <137876422+madhu1703@users.noreply.github.com>
2025-05-24 21:47:43 +0530

    UC1Lombok added

--- EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
@@ -1,12 +1,17 @@
 package com.bridgelabz.EmployeePayrollApp.dto;
-import jakarta.validation.constraints.NotNull;
+
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
 
-    @NotNull(message = "Name should not be null")
+    @NotBlank(message = "Name should not be blank or null")
     private String name;
 
     @Min(value = 500, message = "Salary must be at least 500")
