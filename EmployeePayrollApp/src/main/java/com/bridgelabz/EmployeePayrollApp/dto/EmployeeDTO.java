fb9f5a313d638aa25f3e257dc50d9fedf92e43a9
Madhumitha A <137876422+madhu1703@users.noreply.github.com>
2025-05-25 12:36:43 +0530

    added UC1Validation

--- EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
@@ -2,6 +2,8 @@ package com.bridgelabz.EmployeePayrollApp.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,4 +18,7 @@ public class EmployeeDTO {
 
     @Min(value = 500, message = "Salary must be at least 500")
     private double salary;
+    @NotEmpty(message = "Name must not be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
+    private String employeeName;
 }
